ed737659511f260b328296907f91fbdb367001f6
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-12-24 08:15:02 +0800

    initial commit

---
+++ src/test/java/com/cvsuimus/bsit4b/Bsit4bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvsuimus.bsit4b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bsit4bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
