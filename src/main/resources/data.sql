90362cefed7286c149c6ed95938f266eff689150
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2024-01-03 20:10:43 +0800

    chore: update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,3 +29,45 @@ INSERT INTO users (
   'ADMIN', -- role
   '' -- deleted_on
 );
+
+INSERT INTO student (
+  id, 
+  student_number,
+  first_name,
+  middle_name,
+  last_name,
+  name_suffix,
+  email,
+  contact_number,
+  course
+) VALUES (
+  (select next value for student_seq), -- id
+  '2020108', -- student_number
+  'Juan', -- first_name
+  '', -- middle_name
+  'Dela Cruz', -- last_name
+  'Jr.', -- name_suffix
+  'juan.delacruzjr@cvsu.edu.ph', -- email
+  '09223456790', -- contact_number
+  'BSIT' -- course
+), (
+  (select next value for student_seq), -- id
+  '2020109', -- student_number
+  'John', -- first_name
+  '', -- middle_name
+  'Doe', -- last_name
+  '', -- name_suffix
+  'john.doe@cvsu.edu.ph', -- email
+  '09223456791', -- contact_number
+  'BSCS' -- course
+), (
+  (select next value for student_seq), -- id
+  '2020110', -- student_number
+  'Jane', -- first_name
+  '', -- middle_name
+  'Smith', -- last_name
+  '', -- name_suffix
+  'jane.smith@cvsu.edu.ph', -- email
+  '09223456792', -- contact_number
+  'BSHM' -- course
+);
